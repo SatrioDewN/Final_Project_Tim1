e02e8675533c2f878769f49abd4e4d59a312f309
BSI80195 <BSI80195@BSINB23F010.bsidomain.com>
2023-09-16 18:24:28 +0700

    Making Test Suites and Test Report Mobile

---
+++ Test Suites/Mobile/Mobile Register.ts
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Mobile Register</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f035c7dd-a903-4c69-ad79-29b10bb13087</testSuiteGuid>
+   <testCaseLink>
+      <guid>2b9eb5ac-3eac-41af-951e-8830b55a8910</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Positive/CDM_1.01-Register new account</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d8b46877-6931-4c1a-a922-d411d1083b4f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Positive/CDM_1.03-Input value into field Nama with all digit alphabetic</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ab95503c-af8a-4af2-a306-408ed3ce0c4b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Positive/CDM_1.07-Fill field Whatsapp with 9 digit numeric</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2fa9cbc3-8fce-4849-8d37-5174fc513fbf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Positive/CDM_1.10-Fill field password with alphanumeric and 8 characters</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9f91bfa4-ff28-4bca-bf5a-7862a3e88795</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Positive/CDM_1.12-Fill field password and confirm password with match each other</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f70959ed-8c28-4baf-9c06-46d82c501fc7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Negative/CDM_1.04-Input value into field nama with alphanumeric</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f411111c-8e0e-4f32-aaae-f64b9e9cbaf0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Negative/CDM_1.05-Input blank in Tanggal lahir field</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b1eec651-c010-482f-81ca-2e8b3a761345</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Negative/CDM_1.06-Register with input field E-mail without domain</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9c7d3e12-097a-4eb4-b7a4-3d9ebab46372</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Negative/CDM_1.08-Fill field Whatsapp with 8 digit numeric</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>433f8c00-93eb-4ad6-a765-2be17aa4878f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Negative/CDM_1.11-Fill field password with alphanumeric and 7 characters</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4f428a7d-f4ae-40c1-82f5-490c76f7e100</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CD Mobile Register/CDM Register Negative/CDM_1.13-Fill field password and confirm password not match</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
